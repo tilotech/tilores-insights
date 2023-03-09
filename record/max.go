49280977cac0576c9ef9768ba5f158944e3dac50
Stefan Berkner <stefan.berkner@tilotech.io>
2023-03-09 15:42:29 +0100

    Improve min and max

--- record/max.go
+++ record/max.go
@@ -1,8 +1,6 @@
 package record
 
 import (
-	"math"
-
 	api "github.com/tilotech/tilores-plugin-api"
 )
 
@@ -10,27 +8,17 @@ import (
 //
 // Returns null if all values are null.
 func Max(records []*api.Record, path string) (*float64, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
-	max := 0.0
-	counted := 0.0
+	var max *float64
 	for _, record := range records {
 		number, err := ExtractNumber(record, path)
 		if err != nil {
 			return nil, err
 		}
 		if number != nil {
-			if counted == 0 {
-				max = *number
-			} else {
-				max = math.Max(max, *number)
+			if max == nil || *number > *max {
+				max = number
 			}
-			counted++
 		}
 	}
-	if counted == 0 {
-		return nil, nil
-	}
-	return pointer(max), nil
+	return max, nil
 }
--- record/min.go
+++ record/min.go
@@ -1,8 +1,6 @@
 package record
 
 import (
-	"math"
-
 	api "github.com/tilotech/tilores-plugin-api"
 )
 
@@ -11,27 +9,17 @@ import (
 // Using min on non-numeric paths will raise an error.
 // Returns null if all values are null.
 func Min(records []*api.Record, path string) (*float64, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
-	min := 0.0
-	counted := 0.0
+	var min *float64
 	for _, record := range records {
 		number, err := ExtractNumber(record, path)
 		if err != nil {
 			return nil, err
 		}
 		if number != nil {
-			if counted == 0 {
-				min = *number
-			} else {
-				min = math.Min(min, *number)
+			if min == nil || *number < *min {
+				min = number
 			}
-			counted++
 		}
 	}
-	if counted == 0 {
-		return nil, nil
-	}
-	return pointer(min), nil
+	return min, nil
 }
